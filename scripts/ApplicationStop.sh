d99631454895dca371f12a06d1e6991276b868fc
renatojcastro <35338469+renatojcastro@users.noreply.github.com>
2021-09-26 02:19:29 -0300

    Update ApplicationStop.sh

--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -2,4 +2,4 @@
 # stop http service
 
 #sudo service httpd stop
-sudo systemctl stop httpd.service --> Essa será a primeira linha a ser executada, parando nosso servidor web
+sudo systemctl stop httpd.service
